2ef50dbe31499c19630c1e105038ab2513e17e48
root <root@MegaLapek.localdomain>
2018-04-28 18:34:47 +0200

    adjust app to deploy on heroku

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
-  useMongoClient: true
-});
+mongoose.connect(
+  'mongodb://admin:strongPassword@ds261969.mlab.com:61969/nodeappdatabase',
+  {
+    useMongoClient: true
+  }
+);
 
 const userSchema = new Schema({
   name: String,
